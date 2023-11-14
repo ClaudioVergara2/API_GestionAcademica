1d64a5605c32575708a2d471f593626fcceed6dd
ClaudioVergara2 <claudio.vergara2@virginiogomez.cl>
2023-11-14 14:28:50 -0300

    [feat] SemestresController: Se listan e Insertar los Semestres

---
+++ Controllers/SemestresController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using API_Notas.Models;
+
+namespace API_Notas.Controllers
+{
+    public class SemestresController : Controller
+    {
+        private readonly EvaluacionContext _context;
+
+        public SemestresController(EvaluacionContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("ListadoSemestre")]
+        public IActionResult ListadoSemestre()
+        {
+            List<Semestre> listado = new List<Semestre>();
+            var sql = from s in _context.Semestres
+                      select new
+                      {
+                          s.IdSemestre,
+                          s.NomSemestre,
+                          s.AnioSemestre,
+                          s.Estado
+                      };
+            foreach (var se in sql)
+            {
+                Semestre semestre = new Semestre();
+                semestre.NomSemestre = se.NomSemestre;
+                semestre.IdSemestre = semestre.IdSemestre;
+                semestre.AnioSemestre= semestre.AnioSemestre;
+                semestre.Estado = semestre.Estado;
+                listado.Add(semestre);
+            }
+            return Ok(listado);
+        }
+        [HttpPost]
+        [Route("InsertarSemestre")]
+        public IActionResult InsertarSemestre(string nom, int anio, int estado)
+        {
+            Semestre st = new Semestre();
+            st.NomSemestre = nom;
+            st.AnioSemestre = anio;
+            st.Estado = estado;
+            _context.Semestres.Add(st);
+            _context.SaveChanges();
+            return Ok("Insertado");
+        }
+    }
+}
