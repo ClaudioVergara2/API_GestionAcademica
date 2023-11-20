4da9ac32d6d51163888f392d8f7492c621daaada
MartoVbos <martin.villalobos@virginiogomez.cl>
2023-11-19 21:04:52 -0300

    [feat]insert/list: Alumnos y Notas

---
+++ Controllers/AlumnoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API_Notas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Notas.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AlumnoController : ControllerBase
+{
+    private readonly EvaluacionContext _context;
+
+    public AlumnoController(EvaluacionContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("ListadoAlumnos")]
+    public IActionResult ListadoAlumnos()
+    {
+        var listado = (from a in _context.Alumnos
+                       select new
+                       {
+                           ID_Alumno = a.IdAlumno,
+                           Rut_Persona = a.RutPersona,
+                           ID_Curso = a.IdCurso
+                       }).ToList();
+
+        return Ok(listado);
+    }
+    [HttpPost]
+    [Route("InsertarAlumno")]
+    public IActionResult InsertarAlumno(string rutPersona, int idCurso)
+    {
+        try
+        {
+            Alumno alumno = new Alumno
+            {
+                RutPersona = rutPersona,
+                IdCurso = idCurso
+            };
+
+            _context.Alumnos.Add(alumno);
+            _context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status200OK, new { respuesta = "Alumno insertado correctamente" });
+        }
+        catch (Exception ex)
+        {
+            var innerMessage = ex.InnerException != null ? ex.InnerException.Message : "No inner exception";
+            // Considera registrar el mensaje en un log para análisis detallado
+            return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Error", respuesta = innerMessage });
+        }
+    }
+}
---
+++ Controllers/NotasController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using API_Notas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace API_Notas.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NotasController : ControllerBase
+{
+    private readonly EvaluacionContext _context;
+
+    public NotasController(EvaluacionContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("ListadoNotas")]
+    public IActionResult ListadoNotas()
+    {
+        var listado = _context.Notas.Select(n => new
+        {
+            ID_Nota = n.IdNota,
+            Nota = n.Nota1,
+            ID_Alumno = n.IdAlumno,
+            Numero_Nota = n.NumNota
+        }).ToList();
+
+        return Ok(listado);
+    }
+
+    [HttpPost]
+    [Route("InsertarNota")]
+    public IActionResult InsertarNota(decimal nota, int idAlumno, int numNota)
+    {
+        try
+        {
+            Nota nuevaNota = new Nota
+            {
+                Nota1 = nota,
+                IdAlumno = idAlumno,
+                NumNota = numNota
+            };
+
+            _context.Notas.Add(nuevaNota);
+            _context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status200OK, new { respuesta = "Nota insertada correctamente" });
+        }
+        catch (Exception ex)
+        {
+            var innerMessage = ex.InnerException != null ? ex.InnerException.Message : "No inner exception";
+            return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Error", respuesta = innerMessage });
+        }
+    }
+}
